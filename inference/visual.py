6d7fa04b874570668817e1b4959fc4a6170c3709
姚正发 <976691141@qq.com>
2022-12-25 20:27:32 +0800

    增加条件过滤与评估

--- inference/visual.py
+++ inference/visual.py
@@ -1,4 +1,5 @@
 import fiftyone as fo
+from fiftyone import ViewField as F
 import torch
 import cv2
 import numpy as np
@@ -13,6 +14,9 @@ def xyxytoxywh(bbox, size):
 
 
 def predictions(view, dataset, model_path):
+    """
+    进行推理，实现将yolo格式标注数据与yolo推理结论进行可视化
+    """
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     classes = dataset.default_classes
     model = torch.hub.load('ultralytics/yolov5:v6.0', 'custom', path=model_path)
@@ -43,8 +47,29 @@ def predictions(view, dataset, model_path):
             sample.save()
 
 
+def filter_view(view):
+    """
+    条件过滤,具备排序与条件
+    """
+    return view.sort_by(F("predictions.detections").length(), reverse=True)\
+        .filter_labels('predictions', F('confidence') > 0.5)
+
+
+def evaluate(dataset):
+    """
+    评估模型
+    """
+    results = dataset.evaluate_detections(
+        'predictions',
+        gt_field="ground_truth",
+        method="open-images",
+        eval_key="yolov5",
+        compute_mAP=True
+    )
+    print('\nmAP: %4.f' % results.mAP())
+
+
 if __name__ == '__main__':
-    fo.config.database_dir = './fiftyonedata'
     name = 'vest_dataset'
     dataset_dir = '../data/vest'
     splits = ["train", "val"]
@@ -61,9 +86,12 @@ if __name__ == '__main__':
                 tags=split,
             )
 
-            dataset.tags = ['vms', 'vest']
-            dataset.save()
-            predictions_view = dataset.take(100, seed=51)
-            predictions(predictions_view, dataset, '../models/vest.pt')
-            session = fo.launch_app(predictions_view, desktop=True)
-            session.show()
+    dataset.tags = ['vms', 'vest']
+    dataset.save()
+    predictions_view = dataset.take(100, seed=2)
+    predictions(predictions_view, dataset, '../models/vest.pt')
+
+    evaluate(dataset)
+
+    session = fo.launch_app(filter_view(predictions_view), desktop=True)
+    session.wait()
